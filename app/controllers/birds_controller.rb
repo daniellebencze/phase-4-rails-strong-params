d4254ae983ee2d552e8ddba08f5a5e5161bef6a1
Danielle Bencze <daniellebencze@live.com>
2022-03-15 10:35:38 -0700

    commit tree 148b7d3f785a7c9a4242a6ecd562e18c6e532b97

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
 
+    wrap_parameters format: []
+
   # GET /birds
   def index
     birds = Bird.all
@@ -7,9 +9,31 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
+  # def create
+  #   bird = Bird.create(name: params[:name], species: params[:species])
+  #   render json: bird, status: :created
+  # end
+
+#   def create
+#   bird = Bird.create(params)
+#   render json: bird, status: :created
+# end
+#method above will not work since a malicious user can potentially update attributes we dont want to give them access to 
+
+
+  # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
+  end
+
+  # other controller actions here
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
   # GET /birds/:id
